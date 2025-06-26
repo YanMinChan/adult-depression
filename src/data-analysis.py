ef04c93e0951fa41f1050908a660075e46f303e6
Yan Min <yc89@hw.ac.uk>
2025-06-26 23:56:09 +0100

    wip: Plot the descriptive statistics of gender and depression

--- src/data-analysis.py
+++ src/data-analysis.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sb
 
 # Load data
 df = pd.read_csv('./data/adult-depression-data-cleaned.csv')
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(df)
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#   print(df)
+# print(df.dtypes)
 
-# Line plot, scatter plot
+## Descriptive statistics analysis
+# Higest depression rate (gender) by year
+df_gender = df[["Year", "Male", "Female"]]
+genders = ["Male", "Female"]
+counts = df_gender[df_gender["Year"]==2012][["Male", "Female"]].values[0].tolist()
+
+fig, axs = plt.subplots(1, 1)
+axs.bar(genders, counts)
+
+plt.show()
+
 
